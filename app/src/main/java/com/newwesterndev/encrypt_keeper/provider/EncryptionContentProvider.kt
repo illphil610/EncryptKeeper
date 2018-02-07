0bb2a40f69ff2b8ba9481edc98d9b6e0b3d60131
illphil610 <pcappelli901@gmail.com>
2018-02-06 19:32:50 -0500

    Converted auto generated java provider file to kotlin.

---
+++ app/src/main/java/com/newwesterndev/encrypt_keeper/provider/EncryptionContentProvider.kt
@@ -0,0 +1,44 @@
+package com.newwesterndev.encrypt_keeper.provider
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.MatrixCursor
+import android.net.Uri
+
+class EncryptionContentProvider : ContentProvider() {
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        // Implement this to handle requests to delete one or more rows.
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun getType(uri: Uri): String? {
+        // TODO: Implement this to handle requests for the MIME type of the data
+        // at the given URI.
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        // TODO: Implement this to handle requests to insert a new row.
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun onCreate(): Boolean {
+        // TODO: Implement this to initialize your content provider on startup.
+        return false
+    }
+
+    override fun query(uri: Uri, projection: Array<String>?, selection: String?,
+                       selectionArgs: Array<String>?, sortOrder: String?): Cursor? {
+        val matrixCursor = MatrixCursor(arrayOf("key", "value"))
+        matrixCursor.addRow(arrayOf("public", "private"))
+        return matrixCursor
+    }
+
+    override fun update(uri: Uri, values: ContentValues?, selection: String?,
+                        selectionArgs: Array<String>?): Int {
+        // TODO: Implement this to handle requests to update one or more rows.
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+}
